8bfbb48de6dc6087427184f48f8c8a9b666f62d6
Kenny Jean <kennyjean561@gmail.com>
2021-01-06 21:58:18 -0600

    game completed

--- js/main.js
+++ js/main.js
@@ -1,42 +1,51 @@
-let input = document.getElementById('input');
-let btnIncrease = document.getElementById('btnIncrease');
-let btnDecrease = document.getElementById('btnDecrease');
-let countUp = 0;
-let updated = 0;
-let value = 0; 
-let count = [];
+
+// get dom elements by id
+    // get display
+    const display = document.getElementById('display');
+    // get btnIncrease
+    const incrementButton = document.getElementById('increment-btn');
+    // get input
+    const inputValue = document.getElementById('input-value');
+    // get btnDecrease
+    const decrementButton = document.getElementById('decrement-btn');
+    
+// add click event listeners to the buttons
+incrementButton.addEventListener('click', addToCount)
+decrementButton.addEventListener('click', subtractFromCount)
+// create variable for running total
+let total = 0
 
 
-
-
-function increase() {
-    input = count += value;
-}
-
-function decrease() {
-    input = count -= value;
-}
-
-document.addEventListener("DOMContentLoaded", ()=> {
-    console.log("🥶Freezing");
-    // Upon Loading
-    // input value should be 1
-    input = 0;
-    // initial count 0
-    count = 0;
+//called by '+' button event lister
+function addToCount() {
     // When the "+" button is clicked
-    function count() { 
-        
-        if( input < value) {
-            countUp = btnIncrease.target.addEventListener('click', increase);
-            console.log(countUp)
-        }    
-        // When button is clicked, the value entered in input is added to count
-    //When the "-" button id clicked
-    countDown = btnDecrease.target.addEventListener('click', decrease);
-    }
-        // When button is clicked, the value entered in input is subtracted from count
-    // display new count
+    let fromString = parseInt(inputValue.value)
+    total = total + fromString
+// When button is clicked, the value entered
+//  in input is added to count and the new 
+//  count value displayed
+    updateCount()
+}
 
 
-})
+// called by '-' button event lister
+function subtractFromCount() {
+// When button is clicked, 
+let fromString = parseInt(inputValue.value)
+    total = total - fromString
+// when clicked the value entered in input is 
+// subtracted from count
+
+    updateCount()
+}
+
+
+//called to update count on screen
+function updateCount(){
+    // update display with running total
+    display.innerText = total 
+}
+
+
+
+
